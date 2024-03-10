7b28aaee525a946acdf6362e68436c977c37e335
mnicolas94 <miguelcok27@gmail.com>
2024-03-10 15:33:11 -0400

    feat: implement ConditionalEvent

---
+++ Runtime/Behaviours/ConditionalEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace SerializableCallback.Behaviours
+{
+    public class ConditionalEvent : MonoBehaviour
+    {
+        [SerializeField] private SerializableValueCallback<bool> _condition;
+        [SerializeField] private bool _negate;
+        [SerializeField] private SerializableEvent _event;
+
+        public void Invoke()
+        {
+            if (_negate ^ _condition.Value)
+            {
+                _event.Invoke();
+            }
+        }
+    }
+}
